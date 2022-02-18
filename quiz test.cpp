eeb303fcfc002bc5b1056dfdf689668e48980cfa
Shivani Kashyap <81462774+Kshivi@users.noreply.github.com>
2022-02-18 10:02:20 +0530

    Update quiz test.cpp

--- quiz test.cpp
+++ quiz test.cpp
@@ -3,6 +3,7 @@
 #include<string.h>
 #include<conio.h>
 #include<fstream>
+#include<windows.h>
 #include<dos.h>
 float v;          //global declaration
 using namespace std;
@@ -10,7 +11,7 @@ using namespace std;
 class quiztest
 {
     private:
-    	static int count;       //static data member
+    	
         int roll;
         float x;
     char name[20];
@@ -23,20 +24,14 @@ class quiztest
 	  x=0; 
 	}
     void putdata();
-    void quiz1();              //students having odd rollnumbers
-    void quiz2();				//students having even rollnumbers
+    void quiz1();              //students choose oops in c++
+    void quiz2();				//students choose python
+    void quiz3();				//students choose java
     void display();
-    void getcount(void)
-    {
-    	cout<<"\n\n\n\t\t\t\t\t\t\t\t\t\t\t$$$$$$$$$$***WHOLE QUIZ DATA***$$$$$$$$$$\n";
-    	cout<<"\n\t\t\t\t\t\t\t\t\t\t\t\t"<<count<<" Student given this quiz out of 3"<<"\n";
-    	
-	}
+   
 };
-int quiztest::count;		//defination of static data member
 void  quiztest::quiz1()
 {
-	count++;
    ifstream in;
    char a;
       int i=1;
@@ -69,7 +64,7 @@ void  quiztest::quiz1()
 }
 void  quiztest:: quiz2()
 {
-	count++;
+	
    ifstream in;
    char a;
       int i=1;
@@ -100,11 +95,44 @@ void  quiztest:: quiz2()
     }
    in.close();
 }
+void  quiztest::quiz3()
+{
+   ifstream in;
+   char a;
+      int i=1;
+   in.open("D:/java.txt");
+    while(in.eof()==0)
+    {
+    in.getline(s,100);
+    cout<<endl<<s;
+   if(i==5)
+   {
+   in.get(a);
+   cout<<"\nans:";
+    cin>>ans;
+   i=-1;
+    if(ans==a)
+       {
+       x=x+20;
+       cout<<" *Your ans  correct.\n";
+       }
+    else
+       {
+       if(x>0)
+        x=x-10;
+       cout<<" # Wrong ans.\n";
+       }
+    }
+    i++;
+    }
+   in.close();
+}
+
 void quiztest :: putdata()
 {
 	cout<<"\tENTER YOUR DETAILS::\n";
      cout<<"\tName:";
-     cin>>name;
+     gets(name);
      cout<<"\troll no:";
      cin>>roll;
      cout<<"\tenter your class..your class should be  btechcs/btechit:";
@@ -126,47 +154,65 @@ void quiztest :: display()
 	}
     cout<<"\n\n\tTHANKS FOR GIVING THIS QUIZ";
 }
-const int size=3;    //constant variable
+
 int main()
 {
 	system("color 7d");
-    int i;
-    quiztest q[size]; 
-    cout<<"\n\t\t\t\t\t\t\t\t\t\t**********WELCOME TO THE QUIZ TEST of OOPS BY***************";
+	 quiztest q;
+	 
+    cout<<"\n\t\t\t\t\t\t\t\t\t\t**********WELCOME TO THE QUIZ TEST ***************";
 	cout<<"\n\t\t\t\t\t\t\t\t\t\t\t\t************SHIVANI KASHYAP*************";
 	cout<<endl;
-	for(i=0;i<size;i++)
-	{
-		cout<<"\n\n\t\t\t\t~~~~~~~~~*** QUIZ CONTEST ***~~~~~~~~~\t\tt\t\t\tTotal Marks:100\n\n";
-    	q[i].putdata();
-    	cout<<"\n\t20 marks per Ques(ten negative marking for wrong ans)";
-   	    cout<<"\n\n\tselect your choice:\n"
-        <<"\n\t\t1 for start test"
-        <<"\n\t\t0 for quit the test:";
+	cout<<"\n\n\t\t\t\t~~~~~~~~~*** QUIZ CONTEST ***~~~~~~~~~\t\tt\t\t\tTotal Marks:100\n\n";
+	q.putdata();
+	system("CLS");
+	    int ch;
+	
+		cout<<"\n\n\n\t\t\tyou want to continue the  quiz enter the choice "<<"\n";
+		cout<<"1.start the quiz";
+		cout<<"\n0.end the quiz"<<"\n";
+	    cin>>ch;
+	while(ch){
+	
+	    cout<<"\n\t\t20 marks per Ques(ten negative marking for wrong ans)";
+	    cout<<"\n\t\tenter the answere in a,b,c or d";
+        cout<<"\n\n\t which subject you want to give quiz select your choice:\n"
+        <<"\n\t\t1) OOPS in C++:"
+        <<"\n\t\t2) PYTHON"
+        <<"\n\t\t3) JAVA"<<"\n";
         int c;
         cin>>c;
         switch(c)
          {
-            case 1:
-            	if(v==1)
-            	{
-            		q[i].quiz1();    //odd rollno
-        	        q[i].display();
-            	}
-            	else
-            	{
-            		q[i].quiz2();   //even rollno
-        	        q[i].display();
-				}
-        	    break;
-       		case 0:
-       			cout<<"\n\t ok! zero marks assigned you on your quiz test";
-       			break;
+            case 1:{
+			          q.quiz1();
+			          cout<<"\n\n..................YOUR OOPS IN C++ QUIZ RESULT.................\n\n";
+                      q.display();
+                       break;
+                     
+			        }
+        	case 2:{
+				
+			
+        		   q.quiz2();
+        		   cout<<"\n\n......................YOUR PYTHON QUIZ RESULT........................\n\n";
+        		   q.display();
+        		   break;
+        	}
+       		case 3:{
+				
+       			  q.quiz3();
+       			  cout<<"\n\n......................YOUR JAVA QUIZ RESULT........................\n\n";
+       			  q.display();
+       			  break;
+       		}
        		default:
-        		cout<<"\ninvalid choice! enter the choice between 0 or 1";
+        		cout<<"\ninvalid choice! enter the choice between 1,2,3 ";
        	
          }
-	}
-	q[i].getcount();
-    
+	    	cout<<"\n\n\n\t\t\t\tyou want to continue the  quiz enter the choice "<<"\n";
+		    cout<<"1.start the quiz";
+		    cout<<"\n0.end the quiz"<<"\n";
+	    cin>>ch;
+    }
 } 
